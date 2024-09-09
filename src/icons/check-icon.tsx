8f586665005e06e9178dbb188142146a3dadda7d
keroken <kenichi.shinagawa@gmail.com>
2024-09-09 23:55:19 +0900

    add check icon

---
+++ src/icons/check-icon.tsx
@@ -0,0 +1,5 @@
+export const CheckIcon = (
+  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20 6L9 17L4 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+);
