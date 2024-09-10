1ca2ce9030cd812b6e22e7d9f7c08f1f1d8bbbd1
keroken <kenichi.shinagawa@gmail.com>
2024-09-10 23:56:55 +0900

    update Button story

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -2,11 +2,23 @@ import type{ Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
 
-const meta: Meta<typeof Button> = {
+const meta = {
   component: Button,
-};
+  tags: ['autodocs'],
+  args: { onClick: () => alert('clicked!') },
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Dev: Story = {};
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+  },
+};
