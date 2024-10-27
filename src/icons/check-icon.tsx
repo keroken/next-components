22cc98b62d46ef53e4e55cdfc81da64b41cf9535
keroken <kenichi.shinagawa@gmail.com>
2024-10-27 23:39:44 +0900

    fix attribute names in svg

--- src/icons/check-icon.tsx
+++ src/icons/check-icon.tsx
@@ -1,5 +1,5 @@
 export const CheckIcon = (
   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M20 6L9 17L4 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M20 6L9 17L4 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
